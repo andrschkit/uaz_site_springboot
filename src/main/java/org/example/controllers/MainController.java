3c44bb20d1bcf4b8a96cec7e886596989df1a2c0
andrschkit <andrschkit@gmail.com>
2023-10-19 12:15:19 +0300

    Started commit

---
+++ src/main/java/org/example/controllers/MainController.java
@@ -0,0 +1,12 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        return "index";
+    }
+}
